61e283577a611150dc21e6912b3f74541f06ef0c
Elves Sousa <elves.sousa@gmail.com>
2021-07-22 16:21:53 -0300

    Chapter III - Static Pages

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import styles from './styles.module.scss';
+import Link from 'next/link';
+import Prismic from '@prismicio/client';
 
-export default function Posts() {
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+import { RichText } from 'prismic-dom';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+type PostProps = {
+  posts: Post[];
+};
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -10,38 +27,47 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>31 de junho de 2021</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              laudantium sapiente autem rerum quos voluptates natus, minus esse
-              vero minima inventore doloremque suscipit, earum facilis
-              reiciendis, aut ab consequatur temporibus.
-            </p>
-          </a>
-          <a>
-            <time>31 de junho de 2021</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              laudantium sapiente autem rerum quos voluptates natus, minus esse
-              vero minima inventore doloremque suscipit, earum facilis
-              reiciendis, aut ab consequatur temporibus.
-            </p>
-          </a>
-          <a>
-            <time>31 de junho de 2021</time>
-            <strong>Lorem ipsum dolor sit amet</strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              laudantium sapiente autem rerum quos voluptates natus, minus esse
-              vero minima inventore doloremque suscipit, earum facilis
-              reiciendis, aut ab consequatur temporibus.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        { day: '2-digit', month: 'long', year: 'numeric' }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
