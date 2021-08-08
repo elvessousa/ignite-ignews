e936d4377df38fcb4e9de067bac7cef1577f5d70
Elves Sousa <elves.sousa@gmail.com>
2021-08-08 20:14:06 -0300

    Chapter V: First test configurations

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { ActiveLink } from '.';
 
-/* jest.mock('next/router', () => {
+jest.mock('next/router', () => {
   return {
     useRouter: () => {
       return {
@@ -9,9 +9,7 @@ import { ActiveLink } from '.';
       };
     },
   };
-}); */
-
-const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+});
 
 test('active link renders correctly', () => {
   const { debug } = render(
